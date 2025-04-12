5194494f636d676580614dafa71813d8435705f2
Suleyman Citil <189617955+c-odebroker@users.noreply.github.com>
2025-04-11 19:24:29 -0500

    Implement 'Strike' button for strikethrough items in the list but something wrong
    Signed-off-by: c-odebroker <189617955+c-odebroker@users.noreply.github.com>

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ def get_todos(filepath=FILEPATH):
     """
     Read a text file and return the list of to-do items.
     """
-    with open(filepath, 'r') as file_local:
+    with open(filepath, 'r', encoding='utf-8') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
@@ -13,8 +13,14 @@ def write_todos(todos_arg,filepath=FILEPATH):
     """
     Write the to-do items list in the text file
     """
-    with open(filepath, 'w') as file:
+    with open(filepath, 'w', encoding='utf-8' ) as file:
         file.writelines(todos_arg)
+
+
+def strike(text):
+    return ''.join(char + '\u0336' for char in text)
+
+
 
 if __name__ == "__main__":
  print("Hello")
--- gui.py
+++ gui.py
@@ -2,25 +2,28 @@ import FreeSimpleGUI as sg
 
 import functions
 
+
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("ADD")
+input_box = sg.InputText(tooltip="Enter todo", key='todo', )
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45,10))
+
+add_button = sg.Button("ADD")
 edit_button = sg.Button("Edit")
-compete_button = sg.Button("Complete")
+complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
+strikethrough_button = sg.Button("Strike")
 
 window = sg.Window('My To-Do App',
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box, edit_button, compete_button],
+                           [list_box, edit_button, complete_button, strikethrough_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
     print(1, event)
-    print(2, values)
+    print(2, values['todo'])
     print(3, values['todos'])
     match event:
         case "ADD":
@@ -29,6 +32,7 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+            window['todo'].update(value='')
 
         case "Edit":
             todo_to_edit = values['todos'][0]
@@ -39,6 +43,17 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        #case "Strike":
+        #    todo_to_markoff = values['todos'][0]
+        #
+        #    todos = functions.get_todos()
+        #    index = todos.index(todo_to_markoff)
+        #    todos[index] = functions.strike(todo_to_markoff)
+        #    functions.write_todos(todos)
+        #
+        #    window['todos'].update(values=todos)
+        #    window['todo'].update(value='')
 
         case "Complete":
             todo_to_complete = values['todos'][0]
